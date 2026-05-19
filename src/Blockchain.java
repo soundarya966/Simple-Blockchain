5a62a4b1ab188b9a40a7a9d3e38d09014208572f
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 21:20:47 +0530

    Create Blockchain.java

---
+++ src/Blockchain.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Blockchain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 4;
+
+    public static Boolean isChainValid() {
+
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        for (int i = 1; i < blockchain.size(); i++) {
+
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
